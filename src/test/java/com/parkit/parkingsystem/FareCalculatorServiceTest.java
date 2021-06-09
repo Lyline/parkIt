e4b8fb9c1fc8511e787b31fa7d895f90e9da9210
Lyline <greg.quesne@gmail.com>
2021-06-09 15:02:14 +0200

    TU - Green - calculateFareCareWithLessThirtyMinutes()

--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -139,4 +139,17 @@ public class FareCalculatorServiceTest {
         assertEquals((44.50*Fare.CAR_RATE_PER_HOUR),ticket.getPrice());
     }
 
+    @Test
+    public void calculateFareCareWithLessThirtyMinutes(){
+        LocalDateTime inTime=new LocalDateTime();
+        inTime=inTime.minusMinutes(29);
+        LocalDateTime outTime=new LocalDateTime();
+        ParkingSpot parkingSpot = new ParkingSpot(1,ParkingType.CAR,false);
+
+        ticket.setInTime(inTime);
+        ticket.setOutTime(outTime);
+        ticket.setParkingSpot(parkingSpot);
+        fareCalculatorService.calculateFare(ticket);
+        assertEquals(0,ticket.getPrice());
+    }
 }
