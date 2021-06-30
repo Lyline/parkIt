21b2b32cc1d55276f1a79efe46a6347187ae5093
Lyline <greg.quesne@gmail.com>
2021-06-30 12:13:56 +0200

    ADD - Tests ParkingServiceTest.java

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -3,11 +3,12 @@ package com.parkit.parkingsystem;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import org.apache.logging.log4j.Logger;
 import org.joda.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -23,8 +27,9 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class ParkingServiceTest {
 
+  private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+  private static DataBasePrepareService dataBasePrepareService;
   private static ParkingService parkingService;
-  private static Logger logger;
 
   @Mock
   private static InputReaderUtil inputReaderUtil;
@@ -33,11 +38,13 @@ public class ParkingServiceTest {
   @Mock
   private static TicketDAO ticketDAO;
   @Mock
-  ParkingSpot parkingSpot;
+  private ParkingSpot parkingSpot;
 
 
   @BeforeEach
-  private void setUpPerTest() {
+  private void setUpPerTest() throws SQLException, ClassNotFoundException {
+    Connection con = dataBaseTestConfig.getConnection();
+    //dataBasePrepareService.clearDataBaseEntries();
     parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
   }
 
@@ -61,17 +68,16 @@ public class ParkingServiceTest {
     assertEquals(new ParkingSpot(4, ParkingType.BIKE, false), parkingSpot);
   }
 
-    /*@Test
-    public void getNextParkingNumberIfNotAvailableTest(){
-      when(inputReaderUtil.readSelection()).thenReturn(2);
-      when(parkingSpotDAO.getNextAvailableSlot(ParkingType.BIKE)).thenReturn(-1);
+  @Test
+    public void getNextParkingNumberIfNotAvailableTest() {
 
-     //parkingService.getNextParkingNumberIfAvailable();
-      Exception exception = null;
-      assertThrows(Exception.class,()-> parkingService.getNextParkingNumberIfAvailable());
-      assertEquals("Error fetching parking number from DB. Parking slots might be full",exception.getMessage());
-    }
-*/
+    when(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).thenReturn(0);
+
+
+    assertThrows(Exception.class, () -> parkingService.getNextParkingNumberIfAvailable(), "Error fetching parking number from DB. Parking slots might be full");
+    //assertEquals(,exception.getMessage());
+  }
+
 
   @Test
   public void processValidIncomingVehicleTest() throws Exception {
@@ -126,6 +132,6 @@ public class ParkingServiceTest {
     ticketDAO.getTicket(null);
     ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
     parkingService.processExitingVehicle();
-    assertThrows(Exception.class, () -> logger.error("Unable to process exiting vehicle"));
+    //assertThrows(Exception.class, () -> logger.error("Unable to process exiting vehicle"));
   }
 }
