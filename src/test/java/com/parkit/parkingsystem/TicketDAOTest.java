a9f35ac4d1ce3ad4bb1694fc26008ddb35524ed1
Lyline <greg.quesne@gmail.com>
2021-07-07 12:19:51 +0200

    REFACT/CLEAN - TicketDAOTest() and ADD exception tests

--- src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/TicketDAOTest.java
@@ -6,6 +6,7 @@ import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
+import org.apache.logging.log4j.Logger;
 import org.joda.time.LocalDateTime;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static junit.framework.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 @ExtendWith(MockitoExtension.class)
 public class TicketDAOTest {
@@ -21,6 +24,7 @@ public class TicketDAOTest {
   private static DataBasePrepareService dataBasePrepareService = new DataBasePrepareService();
   private TicketDAO ticketDAO;
   private ParkingSpot parkingSpot;
+  private Logger logger;
 
   @Mock
   private Ticket ticket;
@@ -29,47 +33,44 @@ public class TicketDAOTest {
   private void setUpPerTest() {
     ticketDAO = new TicketDAO();
     ticketDAO.dataBaseConfig = dataBaseTestConfig;
+    dataBasePrepareService.clearDataBaseEntries();
     ticket = new Ticket();
     parkingSpot = new ParkingSpot(1, ParkingType.CAR, false);
   }
 
   @Test
-  public void saveIncomingTicketTest() {
-    dataBasePrepareService.clearDataBaseEntries();
-
+  public void saveIncomingTicketValidateTest() {
     LocalDateTime inTime = new LocalDateTime("2020-06-01T00:00:00");
-
     ticket.setVehicleRegNumber("MyCar");
     ticket.setParkingSpot(parkingSpot);
     ticket.setInTime(inTime);
     ticket.setOutTime(null);
 
-    boolean result = ticketDAO.saveTicket(ticket);
-    assertFalse(result);
+    assertTrue(ticketDAO.saveTicket(ticket));
   }
 
   @Test
   public void saveOutTicketTest() {
-    dataBasePrepareService.clearDataBaseEntries();
-
     LocalDateTime inTime = new LocalDateTime("2020-06-01T00:00:00");
     LocalDateTime outTime = new LocalDateTime("2020-06-01T01:00:00");
-
     ticket.setVehicleRegNumber("MyCar");
     ticket.setParkingSpot(parkingSpot);
     ticket.setInTime(inTime);
     ticket.setOutTime(outTime);
 
-    boolean result = ticketDAO.saveTicket(ticket);
-    assertFalse(result);
+    assertTrue(ticketDAO.saveTicket(ticket));
+  }
+
+  @Test
+  public void saveIncomingTicketNotValidateTest() {
+    ticketDAO.saveTicket(ticket);
+
+    assertThrows(Exception.class, () -> logger.error("Error fetching next available slot"));
   }
 
   @Test
   public void getTicketAvailableTest() {
-    dataBasePrepareService.clearDataBaseEntries();
-
     LocalDateTime inTime = new LocalDateTime().minusHours(1);
-
     ticket.setVehicleRegNumber("MyGreatCar");
     ticket.setParkingSpot(parkingSpot);
     ticket.setInTime(inTime);
@@ -84,12 +85,16 @@ public class TicketDAOTest {
   }
 
   @Test
-  public void updateTicketAvailableTest() {
-    dataBasePrepareService.clearDataBaseEntries();
+  public void getTicketNotAvailableTest() {
+    ticketDAO.getTicket("");
 
+    assertThrows(Exception.class, () -> logger.error("Error fetching next available slot"));
+  }
+
+  @Test
+  public void updateTicketAvailableTest() {
     LocalDateTime inTime = new LocalDateTime().minusHours(1);
     LocalDateTime outTime = new LocalDateTime();
-
     ticket.setVehicleRegNumber("MyGreatCar");
     ticket.setParkingSpot(parkingSpot);
     ticket.setInTime(inTime);
@@ -104,12 +109,16 @@ public class TicketDAOTest {
   }
 
   @Test
-  public void searchVehicleSubscribeTrueTest() {
-    dataBasePrepareService.clearDataBaseEntries();
+  public void updateTicketNotAvailableTest() {
+    ticketDAO.updateTicket(ticket);
 
+    assertThrows(Exception.class, () -> logger.error("Error saving ticket info"));
+  }
+
+  @Test
+  public void searchVehicleSubscribeTrueTest() {
     LocalDateTime inTime = new LocalDateTime().minusHours(2);
     LocalDateTime outTime = new LocalDateTime().minusHours(1);
-
     ticket.setVehicleRegNumber("MyGreatCar");
     ticket.setParkingSpot(parkingSpot);
     ticket.setInTime(inTime);
@@ -119,6 +128,13 @@ public class TicketDAOTest {
     ticketDAO.saveTicket(ticket);
 
     boolean result = ticketDAO.searchVehicleSubscribe("MyGreatCar");
+
     assertTrue(result);
+  }
+
+  @Test
+  public void searchVehicleSubscribeFalseTest() {
+
+    assertFalse(ticketDAO.searchVehicleSubscribe(""));
   }
 }
